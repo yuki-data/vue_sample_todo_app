c49fcd1355cc1638b5feb802e1b65a66409310b0
yuki-data <45279969+yuki-data@users.noreply.github.com>
2021-04-18 23:41:45 +0900

    新規todoを投稿するmethod

--- main.js
+++ main.js
@@ -8,4 +8,17 @@ const app = new Vue({
       { id: 3, content: "nuxtチュートリアル", state: "not done" },
     ],
   },
+  methods: {
+    submitNewTask() {
+      // ref属性を付けたので$refsから要素を参照できる
+      const content = this.$refs.newtask.value;
+      const id = this.todos.length + 1;
+      if (!content.length) {
+        return;
+      }
+      let todo = { id: id, content: content, state: "not done" };
+      this.todos.push(todo);
+      this.$refs.newtask.value = "";
+    },
+  },
 });
