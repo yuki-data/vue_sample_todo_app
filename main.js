3fb30c82150c9eb622e523b56a2f89a37261f76a
yuki-data <45279969+yuki-data@users.noreply.github.com>
2021-04-18 23:52:54 +0900

    todo.doneの真偽でボタンの表示を切り替え

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const app = new Vue({
   data: {
     // ToDoリストの空の配列
     todos: [
-      { id: 1, content: "rspec勉強", state: "not done" },
-      { id: 2, content: "vueチュートリアル", state: "done" },
-      { id: 3, content: "nuxtチュートリアル", state: "not done" },
+      { id: 1, content: "rspec勉強", done: false },
+      { id: 2, content: "vueチュートリアル", done: true },
+      { id: 3, content: "nuxtチュートリアル", done: false },
     ],
   },
   methods: {
@@ -16,7 +16,7 @@ const app = new Vue({
       if (!content.length) {
         return;
       }
-      let todo = { id: id, content: content, state: "not done" };
+      let todo = { id: id, content: content, done: false };
       this.todos.push(todo);
       this.$refs.newtask.value = "";
     },
